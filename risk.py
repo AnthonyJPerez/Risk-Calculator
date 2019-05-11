155ef6217f1b821b4997e0b7c62d9bc09e1a6547
Anthony Perez <ajperez@Anthonys-MacBook-Air.local>
2019-05-11 00:55:15 -0400

    Updated python simulation implementation to be more functional and python3

--- risk.py
+++ risk.py
@@ -1,103 +1,111 @@
-from random import *
+import random
+import itertools
+from enum import Enum
+
+class RollResult(Enum):
+    Defender = 0
+    Attacker = 1
+    Tie = 2
+
+class DieComparison(Enum):
+    Higher = 1
+    Equal = 0
+    Lower = -1
 
 
+def repeatfunc(func, times=None, *args):
+    """Repeat calls to func with specified arguments.
+
+    Example:  repeatfunc(random.random)
+    """
+    if times is None:
+        return itertools.starmap(func, itertools.repeat(args))
+    return itertools.starmap(func, itertools.repeat(args, times))
 
 
-def compare(attackArmies, defendArmies, attackDice, defendDice):
-  attackArmies -= 0 if attackDice > defendDice else 1
-  defendArmies -= 1 if attackDice > defendDice else 0
-  return (attackArmies, defendArmies)
+#
+# Returns -1, 0, 1 depending on if attacker roll is above, equal to, or below defender roll.
+#
+def compareDice(dice):
+    attacker, defender = dice
+    if attacker > defender:
+        return DieComparison.Higher
+    elif attacker < defender:
+        return DieComparison.Lower
+    return DieComparison.Equal
 
 
+#
+# @param attackerArmies The initial number of armies for the attacker
+# @param attackUntil Keep attacking until the attacker's armies drops at or below this number.
+# @param defenderArmies The initial number of armies for the defender
+# @param defendUntil Keep attacking until the defender's armies drops at or below this number
+# @param ruleset The set of rules that describe how the simulation should run
+#   {
+#       attackerDice: int           - Maximum amount of dice attacker may use
+#       attackerDieSize: Tuple      - Describes (min, max) faces of the attacker die
+#       minArmiesForAttack: int     - Minimum number of armies attacker must have to use at least 1 die.
+#       defenderDice: int           - Maximum amount of dice defender may use
+#       defenderDieSize: Tuple      - Describes (min, max) faces of the defender die
+#       minArmiesForDefend: int     - Minimum number of armies defender must have to use at least 1 die.
+#       tieBehavior: RollResult     - Describes how to handle a tie on a die roll. 0 == defender wins, 1 == attacker wins, 2 == nobody wins
+#   }
+def run_simulation(attackerArmies: int, attackUntil: int, defenderArmies: int, defendUntil: int, ruleset: dict):
+    # Roll the supplied die. Die has a min and max face value
+    roll = lambda dieType: random.randint(dieType[0], dieType[1])
 
+    # Setup the function to compare dice. Comparison is from attacker's point of view.
+    #
+    # Default behavior is highest roll wins, defender wins on ties.
+    rollResult = {
+        DieComparison.Higher: RollResult.Attacker,
+        DieComparison.Lower: RollResult.Defender,
+        DieComparison.Equal: ruleset['tieBehavior']
+    }
 
-def attack_custom(attackArmies, defendArmies, maxNumAttackDice=3, maxNumDefendDice=2, attackDiceMin=1, attackDiceMax=6, defendDiceMin=1, defendDiceMax=6):
-  aArr = []
-  dArr = []
+    # Keep simulating while both the attacker and defenders have armies above the 
+    while attackerArmies > ruleset['minArmiesForAttack'] and attackerArmies > attackUntil and defenderArmies > ruleset['minArmiesForDefend'] and defenderArmies > defendUntil:
+        # generate attacker dice
+        numAttackerDice = attackerArmies - ruleset['minArmiesForAttack']
+        attackerDice = repeatfunc(lambda: ruleset['attackerDieSize'], min(numAttackerDice, ruleset['attackerDice']))
 
-  # Give the players their dice:
-  # Each player gets either the max dice they can use, or dice 
-  # equal to the amount of armies they have to attack with, 
-  # whichever is smallest.
-  [aArr.append(randint(attackDiceMin, attackDiceMax)) for _ in range(min(attackArmies-1, maxNumAttackDice))]
-  [dArr.append(randint(defendDiceMin, defendDiceMax)) for _ in range(min(defendArmies, maxNumDefendDice))]
+        # generate defender dice
+        numDefenderDice = defenderArmies - ruleset['minArmiesForDefend']
+        defenderDice = repeatfunc(lambda: ruleset['defenderDieSize'], min(numDefenderDice, ruleset['defenderDice']))
 
-  # Sort each player's dice
-  aArr.sort()
-  dArr.sort()
+        # for both attackers and defenders:
+        #   [dieType, ...] => [roll, ...]
+        # then sort and zip them together for easy comparison
+        # then convert the zipped die rolls into booleans (True if attacker won)
+        rolls =                                             \
+            map(                                            \
+                lambda result: rollResult[compareDice(result)], \
+                zip(                                        \
+                    sorted(                                 \
+                        map(roll, attackerDice),            \
+                        reverse=True),                      \
+                    sorted(                                 \
+                        map(roll, defenderDice),            \
+                        reverse=True)))
 
-  # Compare dice until one array is out of dice
-  try:
-    while True:
-      attackArmies, defendArmies = compare(attackArmies, defendArmies, aArr.pop(), dArr.pop())
-  except IndexError:
-    pass
+        # Count the wins and losses
+        winRolls, lossRolls = itertools.tee(rolls)
+        wins = len(list(filter(lambda x: x == RollResult.Attacker, winRolls)))
+        losses = len(list(filter(lambda x: x == RollResult.Defender, lossRolls)))
+        
+        # update simulation
+        attackerArmies -= losses
+        defenderArmies -= wins
+    print(f"attackerArmies: {attackerArmies}\ndefenderArmies: {defenderArmies}")
 
-  # Return the updated armies
-  return (attackArmies, defendArmies)
+if __name__ == "__main__":
+    ruleset = {}
+    ruleset['attackerDice'] = 3
+    ruleset['attackerDieSize'] = (1,6)
+    ruleset['minArmiesForAttack'] = 1
+    ruleset['defenderDice'] = 2
+    ruleset['defenderDieSize'] = (1,6)
+    ruleset['minArmiesForDefend'] = 0
+    ruleset['tieBehavior'] = RollResult.Defender
 
-
-
-
-def attack(aArmies, dArmies):
-  a = []
-  d = []
-  
-  # Give the players their dice
-  [a.append(randint(1,6)) for _ in range(min(aArmies-1, 3))]
-  [d.append(randint(1,6)) for _ in range(min(dArmies, 2))]
-	
-  # Sort each player's dice
-  a.sort()
-  d.sort()
-  
-  try:
-    # Attack with dice until one array is empty, Max of two dice to check
-    for _ in range(2):
-      attacker, defender = (a.pop(), d.pop())
-      #print "Attacker: ", attacker, ", Defender: ", defender
-      aArmies -= 0 if attacker > defender else 1
-      dArmies -= 1 if attacker > defender else 0
-  except IndexError:
-    pass
-
-  return (aArmies, dArmies)	
-
-  
-
-
-def run_round(a, d):
-  aArmies = a
-  dArmies = d
-  while aArmies > 1 and dArmies > 0:
-    aArmies, dArmies = attack_custom(aArmies, dArmies)
-  return (True if dArmies == 0 else False, aArmies, dArmies)
-  
-
-
-	
-def run_risk(a, d, numRounds):
-  count = 0;
-  armiesLost = 0
-  defenderLoss = 0
-  totalRounds = numRounds
-  attackerArmies = a
-  defenderArmies = d
-  for round in xrange(1, totalRounds):
-    win, aArmies, dArmies = run_round(attackerArmies,defenderArmies)
-    #print "Round: ", round
-    if win:
-      count += 1
-      armiesLost += attackerArmies - aArmies
-    else:
-      defenderLoss += defenderArmies - dArmies
-
-  print ""
-  print "******** Attacker: ", a, " vs. Defender: ", d, " ********"  
-  print "Attacker Win Chance: %3.f%%" % ((count*100)/totalRounds)
-  print "Average Attacker loss: %3.f%%" % ((armiesLost*100)/max(attackerArmies*count,1))
-  print ""
-  print "Defender Win Chance: %3.f%%" % (100 - ((count*100)/totalRounds))
-  print "Average Defender loss: %3.f%%" % ((defenderLoss*100)/max(defenderArmies*count,1))
-  print ""	
-	
+    run_simulation(30, 0, 30, 0, ruleset)
